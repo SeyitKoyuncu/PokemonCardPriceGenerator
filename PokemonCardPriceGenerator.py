f0d5f8778a5e65e59f7cd2994d66b9f74f4d5840
hasankoroglu <seyit404k@gmail.com>
2023-10-31 12:22:33 +0300

    Added to draw recrangle with mouse and take back to rectangle changes with pressing 'd' from keybord

--- PokemonCardPriceGenerator.py
+++ PokemonCardPriceGenerator.py
@@ -4,6 +4,7 @@ import numpy as np
 
 drawing = False
 ix,iy = -1,-1
+rectangles = []
 
 # define mouse callback function to draw circle
 def draw_rectangle(event, x, y, flags, param):
@@ -14,14 +15,20 @@ def draw_rectangle(event, x, y, flags, param):
       iy = y
    elif event == cv2.EVENT_LBUTTONUP:
       drawing = False
-      cv2.rectangle(img, (ix, iy),(x, y),-1)
-   copy_image = img
+      # Append recrangle coordinates to the stack
+      rectangles.append((ix, iy,x, y))
 
-# Create a black image
+# Redraw rectangles in every frame TODO Will be added rectangle count limitation or changing the this method otherwise with much retangle program will be crash or slower
+def redraw_rectangle(img, rectangles):
+   temp_image = img.copy()
+   for rect in rectangles:
+      cv2.rectangle(temp_image, (rect[0], rect[1]), (rect[2], rect[3]), -1)
+   return temp_image
+
+# Create a imaged
 img = cv2.imread('pikachu1.jpg')
-copy_img = img.copy()
 
-# Create a window and bind the function to window
+# Create a window and bind the function to windowd
 cv2.namedWindow("Pokemon Window")
 
 # Connect the mouse button to our callback function
@@ -29,11 +36,16 @@ cv2.setMouseCallback("Pokemon Window", draw_rectangle)
 
 # display the window
 while True:
-   cv2.imshow("Pokemon Window", img)
+   if(rectangles):
+      temp_image = redraw_rectangle(img = img, rectangles = rectangles)
+      cv2.imshow("Pokemon Window", temp_image)
+   else:
+      cv2.imshow("Pokemon Window", img)
    key = cv2.waitKey(1) & 0xFF
-   if key == 27:
+   if key == 27: # Close the window with pressing 'esc'
       break
-
    elif key == ord('d'):  # Press 'd' to delete the last drawn rectangle
-      img = copy_img
+      if(rectangles):
+         rectangles.pop()
+
 cv2.destroyAllWindows()
